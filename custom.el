780ad8797fc6aa8b75a67a48da8944596a45941a
David Conneely <david.conneely+github@gmail.com>
2016-04-12 15:34:43 +0100

    Merged MB settings onto Windows LT settings, and added more attribution

--- custom.el
+++ custom.el
@@ -6,22 +6,22 @@
  '(column-number-mode t)
  '(custom-enabled-themes (quote (tango-dark)))
  '(custom-file "~/.emacs.d/custom.el")
+ '(default-frame-alist nil)
+ '(global-linum-mode t)
  '(inhibit-startup-echo-area-message "dconneely")
  '(inhibit-startup-screen t)
+ '(initial-frame-alist (quote ((width . 120) (height . 48))))
  '(initial-major-mode (quote text-mode))
  '(initial-scratch-message nil)
  '(menu-bar-mode nil)
  '(ns-right-alternate-modifier nil)
- '(package-archives
-   (quote
-    (("gnu" . "http://elpa.gnu.org/packages/")
-     ("melpa-stable" . "https://stable.melpa.org/packages/")
-     ("melpa" . "https://melpa.org/packages/"))))
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+ '(server-mode t)
+ '(tool-bar-mode nil)
+ '(visible-bell t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "CMU Typewriter Text" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
--- init.el
+++ init.el
@@ -1,11 +1,19 @@
-;; Customize uses this file for its settings.
-(setq custom-file "~/.emacs.d/custom.el")
+;; Easy Customization uses this file for its settings.
+;; See: https://www.gnu.org/software/emacs/manual/html_node/emacs/Saving-Customizations.html and https://www.gnu.org/software/emacs/manual/html_node/elisp/Standard-File-Names.html#Standard-File-Names
+(setq custom-file (locate-user-emacs-file "custom.el"))
 (load custom-file)
 
-;; Ensure lein and sbcl can run.
+;; Ensure lein and sbcl can run on OS X.
+;; Source: https://www.emacswiki.org/emacs/EmacsApp#toc8
 (when (string-equal "darwin" (symbol-name system-type))
   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
   (add-to-list 'exec-path "/usr/local/bin"))
+
+;; Add Stable MELPA package archive (HTTP so works with Emacs on Windows).
+;; See https://stable.melpa.org/#/getting-started
+(require 'package)
+(add-to-list 'package-archives
+  '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 ;; Bootstrap `package.el'.
 (setq package-enable-at-startup nil)
@@ -17,7 +25,8 @@
   (package-install 'use-package))
 
 ;; Use some packages.
+(require 'use-package)
 (use-package rainbow-delimiters
-	     :ensure t
-	     :init
-	     (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
+  :ensure t
+  :init
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
